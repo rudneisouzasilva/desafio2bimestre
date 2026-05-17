711c5e32c9099a834ba0fa809f5fed46679356a2
thekidisalright <dev.alexandresilva@gmail.com>
2026-05-16 22:11:50 -0300

    Refactor Login UI and add ClassErro DLL
    Update project to include a local ClassErro DLL and overhaul the login UI. Changes: removed ClassErro.zip and added lib/ClassErro.dll; updated Desafio_AD_BD.csproj to reference lib\ClassErro.dll with SpecificVersion=false. Redesigned LoginUIL: renamed labels (labelUsuario, labelSenha), added labelSigem and labelTitulo, adjusted control sizes/positions, improved fonts/colors, set fixed single border, disabled maximize, centered start position, added form icon in LoginUIL.resx. Introduced Estilizar() for styling and LoginUIL_Load to finalize layout at runtime.

--- LoginUIL.Designer.cs
+++ LoginUIL.Designer.cs
@@ -28,70 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginUIL));
             this.textBoxLogin = new System.Windows.Forms.TextBox();
             this.textBoxPassword = new System.Windows.Forms.TextBox();
             this.buttonEntrar = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.labelUsuario = new System.Windows.Forms.Label();
+            this.labelSenha = new System.Windows.Forms.Label();
             this.linkLabelInscrever = new System.Windows.Forms.LinkLabel();
             this.label3 = new System.Windows.Forms.Label();
             this.linkLabelMostrar = new System.Windows.Forms.LinkLabel();
+            this.labelSigem = new System.Windows.Forms.Label();
+            this.labelTitulo = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBoxLogin
             // 
+            this.textBoxLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(239)))), ((int)(((byte)(232)))));
             this.textBoxLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxLogin.Location = new System.Drawing.Point(168, 62);
+            this.textBoxLogin.Location = new System.Drawing.Point(45, 133);
             this.textBoxLogin.Name = "textBoxLogin";
-            this.textBoxLogin.Size = new System.Drawing.Size(214, 35);
+            this.textBoxLogin.Size = new System.Drawing.Size(264, 30);
             this.textBoxLogin.TabIndex = 0;
             // 
             // textBoxPassword
             // 
             this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxPassword.Location = new System.Drawing.Point(168, 132);
+            this.textBoxPassword.Location = new System.Drawing.Point(39, 206);
             this.textBoxPassword.Name = "textBoxPassword";
-            this.textBoxPassword.Size = new System.Drawing.Size(214, 35);
+            this.textBoxPassword.Size = new System.Drawing.Size(214, 30);
             this.textBoxPassword.TabIndex = 1;
             // 
             // buttonEntrar
             // 
             this.buttonEntrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonEntrar.Location = new System.Drawing.Point(142, 246);
+            this.buttonEntrar.Location = new System.Drawing.Point(39, 285);
             this.buttonEntrar.Name = "buttonEntrar";
-            this.buttonEntrar.Size = new System.Drawing.Size(135, 49);
+            this.buttonEntrar.Size = new System.Drawing.Size(135, 56);
             this.buttonEntrar.TabIndex = 2;
             this.buttonEntrar.Text = "Entrar";
             this.buttonEntrar.UseVisualStyleBackColor = true;
             this.buttonEntrar.Click += new System.EventHandler(this.buttonEntrar_Click);
             // 
-            // label1
+            // labelUsuario
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(41, 68);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 29);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Usuário";
+            this.labelUsuario.AutoSize = true;
+            this.labelUsuario.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.labelUsuario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(135)))), ((int)(((byte)(128)))));
+            this.labelUsuario.Location = new System.Drawing.Point(32, 94);
+            this.labelUsuario.Name = "labelUsuario";
+            this.labelUsuario.Size = new System.Drawing.Size(77, 25);
+            this.labelUsuario.TabIndex = 4;
+            this.labelUsuario.Text = "Usuário";
             // 
-            // label2
+            // labelSenha
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(41, 138);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 29);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Senha";
+            this.labelSenha.AutoSize = true;
+            this.labelSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSenha.Location = new System.Drawing.Point(40, 166);
+            this.labelSenha.Name = "labelSenha";
+            this.labelSenha.Size = new System.Drawing.Size(70, 25);
+            this.labelSenha.TabIndex = 5;
+            this.labelSenha.Text = "Senha";
             // 
             // linkLabelInscrever
             // 
             this.linkLabelInscrever.AutoSize = true;
             this.linkLabelInscrever.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabelInscrever.Location = new System.Drawing.Point(223, 390);
+            this.linkLabelInscrever.Location = new System.Drawing.Point(35, 364);
             this.linkLabelInscrever.Name = "linkLabelInscrever";
-            this.linkLabelInscrever.Size = new System.Drawing.Size(114, 25);
+            this.linkLabelInscrever.Size = new System.Drawing.Size(96, 20);
             this.linkLabelInscrever.TabIndex = 6;
             this.linkLabelInscrever.TabStop = true;
             this.linkLabelInscrever.Text = "Inscreva-se";
@@ -101,39 +106,71 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(70, 390);
+            this.label3.Location = new System.Drawing.Point(24, 260);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(141, 25);
+            this.label3.Size = new System.Drawing.Size(0, 20);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Não é inscrito?";
             // 
             // linkLabelMostrar
             // 
             this.linkLabelMostrar.AutoSize = true;
             this.linkLabelMostrar.LinkColor = System.Drawing.Color.Black;
-            this.linkLabelMostrar.Location = new System.Drawing.Point(271, 179);
+            this.linkLabelMostrar.Location = new System.Drawing.Point(41, 257);
             this.linkLabelMostrar.Name = "linkLabelMostrar";
-            this.linkLabelMostrar.Size = new System.Drawing.Size(111, 20);
+            this.linkLabelMostrar.Size = new System.Drawing.Size(119, 23);
             this.linkLabelMostrar.TabIndex = 8;
             this.linkLabelMostrar.TabStop = true;
             this.linkLabelMostrar.Text = "Mostrar senha";
             this.linkLabelMostrar.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelMostrar_LinkClicked);
             // 
+            // labelSigem
+            // 
+            this.labelSigem.AutoSize = true;
+            this.labelSigem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(135)))), ((int)(((byte)(128)))));
+            this.labelSigem.Location = new System.Drawing.Point(32, 28);
+            this.labelSigem.Name = "labelSigem";
+            this.labelSigem.Size = new System.Drawing.Size(60, 23);
+            this.labelSigem.TabIndex = 9;
+            this.labelSigem.Text = "SIGEM";
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.labelTitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(42)))));
+            this.labelTitulo.Location = new System.Drawing.Point(32, 46);
+            this.labelTitulo.MaximumSize = new System.Drawing.Size(264, 0);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(252, 111);
+            this.labelTitulo.TabIndex = 10;
+            this.labelTitulo.Text = "Sistema de Gestão de Equipamentos e Manutenção";
+            // 
             // LoginUIL
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(428, 478);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(382, 413);
+            this.Controls.Add(this.labelTitulo);
+            this.Controls.Add(this.labelSigem);
             this.Controls.Add(this.linkLabelMostrar);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.linkLabelInscrever);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelSenha);
+            this.Controls.Add(this.labelUsuario);
             this.Controls.Add(this.buttonEntrar);
             this.Controls.Add(this.textBoxPassword);
             this.Controls.Add(this.textBoxLogin);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 460);
+            this.MinimumSize = new System.Drawing.Size(400, 460);
             this.Name = "LoginUIL";
-            this.Text = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SIGEM";
+            this.Load += new System.EventHandler(this.LoginUIL_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,11 +181,13 @@
         private System.Windows.Forms.TextBox textBoxLogin;
         private System.Windows.Forms.TextBox textBoxPassword;
         private System.Windows.Forms.Button buttonEntrar;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelUsuario;
+        private System.Windows.Forms.Label labelSenha;
         private System.Windows.Forms.LinkLabel linkLabelInscrever;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.LinkLabel linkLabelMostrar;
+        private System.Windows.Forms.Label labelSigem;
+        private System.Windows.Forms.Label labelTitulo;
     }
 }
 
--- LoginUIL.cs
+++ LoginUIL.cs
@@ -17,7 +17,91 @@ namespace Desafio_AD_BD
         public LoginUIL()
         {
             InitializeComponent();
+            Estilizar();
             textBoxPassword.UseSystemPasswordChar = true;
+        }
+
+        private void Estilizar()
+        {
+            // Form
+            this.Size = new Size(400, 460);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.BackColor = ColorTranslator.FromHtml("#FFFFFF");
+            this.Text = "SIGEM";
+
+            // Label SIGEM
+            labelSigem.Font = new Font("Segoe UI", 10);
+            labelSigem.ForeColor = ColorTranslator.FromHtml("#888780");
+            labelSigem.Location = new Point(32, 28);
+
+            // Label título
+            labelTitulo.Font = new Font("Segoe UI", 16);
+            labelTitulo.ForeColor = ColorTranslator.FromHtml("#2C2C2A");
+            labelTitulo.MaximumSize = new Size(264, 0);
+            labelTitulo.AutoSize = true;
+            labelTitulo.Location = new Point(32, 46);
+
+            // Label Usuário
+            labelUsuario.Font = new Font("Segoe UI", 11);
+            labelUsuario.ForeColor = ColorTranslator.FromHtml("#888780");
+
+            // TextBox usuário
+            textBoxLogin.Size = new Size(264, 40);
+            textBoxLogin.BackColor = ColorTranslator.FromHtml("#F1EFE8");
+            textBoxLogin.BorderStyle = BorderStyle.FixedSingle;
+            textBoxLogin.Font = new Font("Segoe UI", 12);
+
+            // Label Senha
+            labelSenha.Font = new Font("Segoe UI", 11);
+            labelSenha.ForeColor = ColorTranslator.FromHtml("#888780");
+
+            // TextBox senha
+            textBoxPassword.Size = new Size(264, 40);
+            textBoxPassword.BackColor = ColorTranslator.FromHtml("#F1EFE8");
+            textBoxPassword.BorderStyle = BorderStyle.FixedSingle;
+            textBoxPassword.Font = new Font("Segoe UI", 12);
+
+            // LinkLabel mostrar senha
+            linkLabelMostrar.Font = new Font("Segoe UI", 11);
+            linkLabelMostrar.LinkColor = ColorTranslator.FromHtml("#888780");
+            linkLabelMostrar.ActiveLinkColor = ColorTranslator.FromHtml("#2C2C2A");
+
+            // Button Entrar
+            buttonEntrar.Size = new Size(264, 40);
+            buttonEntrar.BackColor = ColorTranslator.FromHtml("#1A1A18");
+            buttonEntrar.ForeColor = ColorTranslator.FromHtml("#F1EFE8");
+            buttonEntrar.FlatStyle = FlatStyle.Flat;
+            buttonEntrar.FlatAppearance.BorderSize = 0;
+            buttonEntrar.Font = new Font("Segoe UI", 12);
+
+            // LinkLabel inscreva-se
+            linkLabelInscrever.Font = new Font("Segoe UI", 11);
+            linkLabelInscrever.ForeColor = ColorTranslator.FromHtml("#888780");
+            linkLabelInscrever.LinkColor = ColorTranslator.FromHtml("#2C2C2A");
+
+        }
+
+        private void LoginUIL_Load(object sender, EventArgs e)
+        {
+            labelUsuario.Top = labelTitulo.Bottom + 24;
+            labelUsuario.Left = 32;
+            textBoxLogin.Top = labelUsuario.Bottom + 5;
+            textBoxLogin.Left = 32;
+
+            labelSenha.Top = textBoxLogin.Bottom + 12;
+            labelSenha.Left = 32;
+            textBoxPassword.Top = labelSenha.Bottom + 5;
+            textBoxPassword.Left = 32;
+
+            linkLabelMostrar.Top = textBoxPassword.Bottom + 4;
+            linkLabelMostrar.Left = 32 + 264 - linkLabelMostrar.Width;
+
+            buttonEntrar.Top = linkLabelMostrar.Bottom + 16;
+            buttonEntrar.Left = 32;
+
+            linkLabelInscrever.Top = buttonEntrar.Bottom + 14;
+            linkLabelInscrever.Left = 32;
         }
 
         //Metodo para abrir a tela de inscrição
